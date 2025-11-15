e4726f44fe247047e0b840cee51913d96e028b00
Mang Kelogs <Mang Kelogs@MANG_KELOGS>
2025-11-15 20:22:53 +0800

    save

--- StudentListApp/Controllers/StudentController.cs
+++ StudentListApp/Controllers/StudentController.cs
@@ -3,17 +3,37 @@ using StudentListApp.Models;
 
 namespace StudentListApp.Controllers
 {
-    public class StudentController : Controller
+
+    public class StudentsController : Controller
     {
+        private static List<Student> tempStudents = new()
+        {
+            new Student { Id = 1, Name = "John Santos", Age = 20, Course = "IT" },
+            new Student { Id = 2, Name = "Maria Cruz", Age = 21, Course = "CS" },
+            new Student { Id = 3, Name = "Alex Tan", Age = 19, Course = "IS" }
+        };
+        // Display all students
         public IActionResult Index()
         {
-            List<Student> students = new()
+            return View(tempStudents);
+        }
+
+        // Display Create Form
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // Handle form submission
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
             {
-                new Student { Id = 1, Name = "John Santos", Age = 20, Course = "IT" },
-                new Student { Id = 2, Name = "Maria Cruz", Age = 21, Course = "CS" },
-                new Student { Id = 3, Name = "Alex Tan", Age = 19, Course = "IS" }
-            };
-            return View(students);
+                tempStudents.Add(student);
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
         }
     }
 }
